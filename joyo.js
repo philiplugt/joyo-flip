8449e2458b05c2aa6d8481d3af699e6e8535b87a
pxv8780 <philiplugt@netscape.net>
2024-03-17 14:37:27 +0100

    More design work, and added shuffle

--- joyo.js
+++ joyo.js
@@ -1,5 +1,6 @@
 
 // Implement card flip functionality
+var lastGrade = "1";
 const container = document.querySelector('.container');
 
 const buttons = document.querySelectorAll('.grades button');
@@ -10,8 +11,17 @@ function clicker() {
     setGrades(this.dataset.type);
 }
 
+function clicker2() {
+    setGrades(lastGrade, true);
+}
+
 function flipCard() {
     this.classList.toggle('card-flip');
+}
+
+function colorCard(e) {
+    e.preventDefault();
+    this.classList.toggle('card-color');
 }
 
 function flipCardAll() {
@@ -27,6 +37,7 @@ function flipCardAll() {
 function resetCardAll() {
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => card.classList.remove('card-flip'));
+    cards.forEach(card => card.classList.remove('card-color'));
 }
 
 
@@ -62,22 +73,40 @@ const go3 = document.querySelector('.go3');
 go.addEventListener('click', resetCardAll);
 go2.addEventListener('click', flipCardAll);
 go3.addEventListener('change', changeFont);
+const random = document.querySelector('.rnd');
+random.addEventListener('click', clicker2);
 
-document.addEventListener('DOMContentLoaded', setGrades("1"));
+document.addEventListener('DOMContentLoaded', setGrades(lastGrade));
 
+function shuffle(array) {
+  const result = [], itemsLeft = array.concat([]);
+  while (itemsLeft.length) {
+    const randomIndex = Math.floor(Math.random() * itemsLeft.length);
+    const [randomItem] = itemsLeft.splice(randomIndex, 1); // take out a random item from itemsLeft
+    result.push(randomItem); // ...and add it to the result
+  }
 
+  return result;
+}
 
-function setGrades(grade) {
-    console.log('')
+function setGrades(grade, toShuffle=false) {
+    lastGrade = grade;
     const grade1 = kanjis.filter(k => k['grade'] === grade)
     var count = 0;
-    const gradechars = grade1.sort().map((char) => {
+    var grade1_n;
+    if (toShuffle) {
+        grade1_n = shuffle(grade1);
+    } else {
+        grade1_n = grade1.sort();
+    }
+    //<div class="content-number">${grade !== 'S' ? 'G' : ''}${grade} • #${count}</div>
+    const gradechars = grade1_n.map((char) => {
         count += 1;
         // console.log(char)
         return `
         <div class="card">
             <div class="card-face card-front">
-                <div class="content-number">${grade !== 'S' ? 'G' : ''}${grade} • #${count}</div>
+                
                 <div class="content-front">${char.new_shinjitai}</div>
             </div>
             <div class="card-face card-back">
@@ -89,15 +118,29 @@ function setGrades(grade) {
     container.innerHTML = gradechars;
 
     const cards = document.querySelectorAll('.card');
-    cards.forEach(card => card.addEventListener('click', flipCard));
+    cards.forEach(card => {
+        card.addEventListener('click', flipCard);
+        card.addEventListener('contextmenu', colorCard);
+    });
 }
 
 const slider = document.querySelector('.slider');
 slider.addEventListener('change', changeCardSize);
-slider.addEventListener('mousemove', changeCardSize);
+// slider.addEventListener('mousemove', changeCardSize);
 
 function changeCardSize() {
+    var con = document.querySelectorAll('.container');
+    container.style.gap = `${this.value * 40}px`;
     var cards = document.querySelectorAll('.card');
-    console.log(((this.value - 50) / 10))
-    cards.forEach(card => card.style.width = `${10 + ((this.value - 50) / 10)}%`);
+    var card_front = document.querySelectorAll('.content-front');
+    var card_back = document.querySelectorAll('.content-back');
+    card_front.forEach(front => front.style.fontSize = `${this.value * 84}px`);
+    card_back.forEach(front => front.style.fontSize = `${this.value * 36}px`);
+
+    console.log(this.value);//((this.value - 50) / 10))
+    cards.forEach(card => {
+        card.style.width = `${this.value * 240}px`
+        // card.style.borderWidth = `${this.value * 6}px`
+
+    }); //10 + ((this.value - 50) / 10)}%`);
 }
